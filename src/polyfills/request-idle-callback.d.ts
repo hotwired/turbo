09aa8fed0c00c1d6b65c02971ceff3908504e9b0
Sam Stephenson <sam@hey.com>
2020-12-08 22:04:08 -0500

    Remove unused polyfill

--- src/polyfills/request-idle-callback.d.ts
+++
@@ -1,7 +0,0 @@
-export interface IdleDeadline {
-  readonly didTimeout: boolean
-  timeRemaining(): DOMHighResTimeStamp
-}
-
-export const requestIdleCallback: (cb: (deadline: IdleDeadline) => void) => number
-export const cancelIdleCallback: (handle: number) => void
