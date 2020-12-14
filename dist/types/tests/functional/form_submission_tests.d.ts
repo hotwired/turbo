3a3e1098c49827139fe498d24400d69cd4763453
Sean Doyle <sean.p.doyle24@gmail.com>
2020-12-14 12:52:17 -0500

    dev-builds/6610a92 Read turbo-frame target from Submitter
    https://github.com/hotwired/turbo/tree/6610a923f80a8df3990338b12c8e0dacff138d8c

---
+++ dist/types/tests/functional/form_submission_tests.d.ts
@@ -0,0 +1,10 @@
+import { TurboDriveTestCase } from "../helpers/turbo_drive_test_case";
+export declare class FormSubmissionTests extends TurboDriveTestCase {
+    setup(): Promise<void>;
+    "test standard form submission with redirect response"(): Promise<void>;
+    "test submitter form submission reads button attributes"(): Promise<void>;
+    "test frame form GET submission from submitter with data-turbo-frame"(): Promise<void>;
+    "test frame form POST submission from submitter with data-turbo-frame"(): Promise<void>;
+    "test frame form submission with redirect response"(): Promise<void>;
+    "test frame form submission with stream response"(): Promise<void>;
+}
