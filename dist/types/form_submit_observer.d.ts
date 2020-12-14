3271e8b26cbc27f1e5ce5fc79c28893128ae7400
Sean Doyle <sean.p.doyle24@gmail.com>
2020-12-14 11:54:59 -0500

    dev-builds/6629df9 Read FormSubmission.{method,location} from submitter
    https://github.com/hotwired/turbo/tree/6629df9400f70c157f407540828ef363007abe67

---
+++ dist/types/form_submit_observer.d.ts
@@ -0,0 +1,15 @@
+export interface FormSubmitObserverDelegate {
+    willSubmitForm(form: HTMLFormElement, submitter?: HTMLElement): boolean;
+    formSubmitted(form: HTMLFormElement, submitter?: HTMLElement): void;
+}
+export declare class FormSubmitObserver {
+    readonly delegate: FormSubmitObserverDelegate;
+    started: boolean;
+    constructor(delegate: FormSubmitObserverDelegate);
+    start(): void;
+    stop(): void;
+    submitCaptured: () => void;
+    submitBubbled: (event: Event & {
+        submitter?: HTMLElement;
+    }) => void;
+}
