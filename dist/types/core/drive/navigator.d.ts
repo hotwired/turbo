fcb1de9746cbfdc022ca0133ee7b09cc9d7fd6b1
Sean Doyle <sean.p.doyle24@gmail.com>
2020-12-14 12:40:41 -0500

    dev-builds/d56105d turbo-frame: Navigate frame when intercepting GET
    https://github.com/hotwired/turbo/tree/d56105d6ad10b4159dbd97db1fb66e06ff112ddd

---
+++ dist/types/core/drive/navigator.d.ts
@@ -0,0 +1,31 @@
+import { FetchResponse } from "../../http/fetch_response";
+import { FormSubmission } from "./form_submission";
+import { Location } from "../location";
+import { Visit, VisitDelegate, VisitOptions } from "./visit";
+export declare type NavigatorDelegate = VisitDelegate & {
+    allowsVisitingLocation(location: Location): boolean;
+    visitProposedToLocation(location: Location, options: Partial<VisitOptions>): void;
+};
+export declare class Navigator {
+    readonly delegate: NavigatorDelegate;
+    formSubmission?: FormSubmission;
+    currentVisit?: Visit;
+    constructor(delegate: NavigatorDelegate);
+    proposeVisit(location: Location, options?: Partial<VisitOptions>): void;
+    startVisit(location: Location, restorationIdentifier: string, options?: Partial<VisitOptions>): void;
+    submitForm(form: HTMLFormElement): void;
+    stop(): void;
+    reload(): void;
+    goBack(): void;
+    get adapter(): import("../native/adapter").Adapter;
+    get view(): import("./view").View;
+    get history(): import("./history").History;
+    formSubmissionStarted(formSubmission: FormSubmission): void;
+    formSubmissionSucceededWithResponse(formSubmission: FormSubmission, fetchResponse: FetchResponse): Promise<void>;
+    formSubmissionFailedWithResponse(formSubmission: FormSubmission, fetchResponse: FetchResponse): void;
+    formSubmissionErrored(formSubmission: FormSubmission, error: Error): void;
+    formSubmissionFinished(formSubmission: FormSubmission): void;
+    visitStarted(visit: Visit): void;
+    visitCompleted(visit: Visit): void;
+    get location(): Location;
+}
