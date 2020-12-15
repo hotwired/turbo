523eb82f55c28daf2a6f43dbdde129cf770384c0
Sam Stephenson <sam@hey.com>
2020-12-14 19:01:59 -0500

    dev-builds/93bb7e4 Go ES2017; distribute bundles in multiple formats
    https://github.com/hotwired/turbo/tree/93bb7e46e0293e262dd5aa27e83b57c33b1db8a0

---
+++ dist/types/tests/async_script_tests.d.ts
@@ -0,0 +1,6 @@
+import { TurboTestCase } from "./helpers/turbo_test_case";
+export declare class AsyncScriptTests extends TurboTestCase {
+    setup(): Promise<void>;
+    "test does not emit turbo:load when loaded asynchronously after DOMContentLoaded"(): Promise<void>;
+    "test following a link when loaded asynchronously after DOMContentLoaded"(): Promise<void>;
+}
