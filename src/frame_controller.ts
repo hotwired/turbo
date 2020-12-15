5e8518dd1bdb69f32dd0dce18d26408c92829158
David Heinemeier Hansson <david@loudthinking.com>
2020-12-15 17:11:04 +0100

    Just update the header name, leave id for now

--- src/frame_controller.ts
+++ src/frame_controller.ts
@@ -66,7 +66,7 @@ export class FrameController implements FetchRequestDelegate, FormInterceptorDel
   }
 
   additionalHeadersForRequest(request: FetchRequest) {
-    return { "X-Turbo-Frame": this.id }
+    return { "Turbo-Frame": this.id }
   }
 
   requestStarted(request: FetchRequest) {
