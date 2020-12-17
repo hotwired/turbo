a2cbaa3513df2668e7d0e76e4bc059f0aea82415
Sam Stephenson <sam@hey.com>
2020-12-17 11:36:03 -0500

    dev-builds/0802472 Test <turbo-stream> actions
    https://github.com/hotwired/turbo/tree/08024727bfa4ae717592667a5699327e2eceeb7a

---
+++ dist/types/stream_message.d.ts
@@ -0,0 +1,10 @@
+import { StreamElement } from "./elements/stream_element";
+export declare class StreamMessage {
+    static readonly contentType = "text/html; turbo-stream";
+    readonly templateElement: HTMLTemplateElement;
+    static wrap(message: StreamMessage | string): StreamMessage;
+    constructor(html: string);
+    get fragment(): DocumentFragment;
+    get foreignElements(): StreamElement[];
+    get templateChildren(): Element[];
+}
