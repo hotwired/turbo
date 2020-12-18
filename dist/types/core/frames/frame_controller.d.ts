111a70a81e6a45a752dc5a5670d3f1cba80c1569
Sam Stephenson <sam@hey.com>
2020-12-18 16:38:10 -0500

    dev-builds/f3ad1b8 Update LinkInterceptor to use event.detail
    https://github.com/hotwired/turbo/tree/f3ad1b8898da6841dae5d00f35efd4440ef045f6

---
+++ dist/types/core/frames/frame_controller.d.ts
@@ -0,0 +1,46 @@
+import { FetchRequest, FetchRequestDelegate } from "../../http/fetch_request";
+import { FetchResponse } from "../../http/fetch_response";
+import { FormInterceptor, FormInterceptorDelegate } from "./form_interceptor";
+import { FormSubmission, FormSubmissionDelegate } from "../drive/form_submission";
+import { FrameElement } from "../../elements/frame_element";
+import { LinkInterceptor, LinkInterceptorDelegate } from "./link_interceptor";
+import { Locatable } from "../location";
+export declare class FrameController implements FetchRequestDelegate, FormInterceptorDelegate, FormSubmissionDelegate, LinkInterceptorDelegate {
+    readonly element: FrameElement;
+    readonly linkInterceptor: LinkInterceptor;
+    readonly formInterceptor: FormInterceptor;
+    formSubmission?: FormSubmission;
+    private resolveVisitPromise;
+    constructor(element: FrameElement);
+    connect(): void;
+    disconnect(): void;
+    shouldInterceptLinkClick(element: Element, url: string): boolean;
+    linkClickIntercepted(element: Element, url: string): void;
+    shouldInterceptFormSubmission(element: HTMLFormElement): boolean;
+    formSubmissionIntercepted(element: HTMLFormElement): void;
+    visit(url: Locatable): Promise<void>;
+    additionalHeadersForRequest(request: FetchRequest): {
+        "Turbo-Frame": string;
+    };
+    requestStarted(request: FetchRequest): void;
+    requestPreventedHandlingResponse(request: FetchRequest, response: FetchResponse): void;
+    requestSucceededWithResponse(request: FetchRequest, response: FetchResponse): Promise<void>;
+    requestFailedWithResponse(request: FetchRequest, response: FetchResponse): void;
+    requestErrored(request: FetchRequest, error: Error): void;
+    requestFinished(request: FetchRequest): void;
+    formSubmissionStarted(formSubmission: FormSubmission): void;
+    formSubmissionSucceededWithResponse(formSubmission: FormSubmission, response: FetchResponse): void;
+    formSubmissionFailedWithResponse(formSubmission: FormSubmission, fetchResponse: FetchResponse): void;
+    formSubmissionErrored(formSubmission: FormSubmission, error: Error): void;
+    formSubmissionFinished(formSubmission: FormSubmission): void;
+    private findFrameElement;
+    private loadResponse;
+    private extractForeignFrameElement;
+    private loadFrameElement;
+    private focusFirstAutofocusableElement;
+    private scrollFrameIntoView;
+    private shouldInterceptNavigation;
+    get firstAutofocusableElement(): HTMLElement | null;
+    get id(): string;
+    get enabled(): boolean;
+}
