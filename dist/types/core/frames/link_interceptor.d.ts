38092431b1cf4b2af6f4f39ea232d18524f11f5a
Sam Stephenson <sam@hey.com>
2020-12-18 12:15:09 -0500

    dev-builds/4c388f4 Restrict dev-builds/latest to the main branch
    https://github.com/hotwired/turbo/tree/4c388f42b314681ac4fb4bbb98903b8cbb734a28

---
+++ dist/types/core/frames/link_interceptor.d.ts
@@ -0,0 +1,22 @@
+export interface TurboClickEvent extends Event {
+    target: Element;
+    data: {
+        url: string;
+    };
+}
+export interface LinkInterceptorDelegate {
+    shouldInterceptLinkClick(element: Element, url: string): boolean;
+    linkClickIntercepted(element: Element, url: string): void;
+}
+export declare class LinkInterceptor {
+    readonly delegate: LinkInterceptorDelegate;
+    readonly element: Element;
+    private clickEvent?;
+    constructor(delegate: LinkInterceptorDelegate, element: Element);
+    start(): void;
+    stop(): void;
+    clickBubbled: (event: Event) => void;
+    linkClicked: EventListener;
+    willVisit: () => void;
+    respondsToEventTarget(target: EventTarget | null): boolean | null;
+}
