b5e766a927ac4020117071bf1406eb0ddca0afdc
Santiago Perez Perret <perezperret@gmail.com>
2020-12-24 12:53:57 -0300

    Trigger event after stream render

--- src/elements/stream_element.ts
+++ src/elements/stream_element.ts
@@ -21,6 +21,7 @@ export class StreamElement extends HTMLElement {
       if (this.dispatchEvent(this.beforeRenderEvent)) {
         await nextAnimationFrame()
         this.performAction()
+        this.dispatchEvent(this.afterRenderEvent)
       }
     })()
   }
@@ -76,6 +77,10 @@ export class StreamElement extends HTMLElement {
 
   private get beforeRenderEvent() {
     return new CustomEvent("turbo:before-stream-render", { bubbles: true, cancelable: true })
+  }
+
+  private get afterRenderEvent() {
+    return new CustomEvent("turbo:after-stream-render", { bubbles: true, cancelable: true })
   }
 }
 
