244c93578cb37b9250a8f066d9f1589faa51aa99
James Garbutt <43081j@users.noreply.github.com>
2020-12-25 22:39:00 +0000

    add some jsdoc

--- src/core/index.ts
+++ src/core/index.ts
@@ -9,34 +9,88 @@ const session = new Session
 const { navigator } = session
 export { navigator }
 
+/**
+ * Starts the main session.
+ * This initialises any necessary observers such as those to monitor
+ * link interactions.
+ */
 export function start() {
   session.start()
 }
 
+/**
+ * Registers an adapter for the main session.
+ *
+ * @param adapter Adapter to register
+ */
 export function registerAdapter(adapter: Adapter) {
   session.registerAdapter(adapter)
 }
 
+/**
+ * Performs an application visit to the given location.
+ *
+ * @param location Location to visit (a URL or path)
+ * @param options Options to apply
+ * @param options.action Type of history navigation to apply ("restore",
+ * "replace" or "advance")
+ * @param options.historyChanged Specifies whether the browser history has
+ * already been changed for this visit or not
+ * @param options.referrer Specifies the referrer of this visit such that
+ * navigations to the same page will not result in a new history entry.
+ * @param options.snapshotHTML Cached snapshot to render
+ * @param options.response Response of the specified location
+ */
 export function visit(location: Locatable, options?: Partial<VisitOptions>) {
   session.visit(location, options)
 }
 
+/**
+ * Connects a stream source to the main session.
+ *
+ * @param source Stream source to connect
+ */
 export function connectStreamSource(source: StreamSource) {
   session.connectStreamSource(source)
 }
 
+/**
+ * Disconnects a stream source from the main session.
+ *
+ * @param source Stream source to disconnect
+ */
 export function disconnectStreamSource(source: StreamSource) {
   session.disconnectStreamSource(source)
 }
 
+/**
+ * Renders a stream message to the main session by appending it to the
+ * current document.
+ *
+ * @param message Message to render
+ */
 export function renderStreamMessage(message: StreamMessage | string) {
   session.renderStreamMessage(message)
 }
 
+/**
+ * Removes all entries from the Turbo Drive page cache.
+ * Call this when state has changed on the server that may affect cached pages.
+ */
 export function clearCache() {
   session.clearCache()
 }
 
+/**
+ * Sets the delay after which the progress bar will appear during navigation.
+ *
+ * The progress bar appears after 500ms by default.
+ *
+ * Note that this method has no effect when used with the iOS or Android
+ * adapters.
+ *
+ * @param delay Time to delay in milliseconds
+ */
 export function setProgressBarDelay(delay: number) {
   session.setProgressBarDelay(delay)
 }
--- src/elements/stream_element.ts
+++ src/elements/stream_element.ts
@@ -3,6 +3,24 @@ import { nextAnimationFrame } from "../util"
 
 // <turbo-stream action=replace target=id><template>...
 
+/**
+ * Renders updates to the page from a stream of messages.
+ *
+ * Using the `action` attribute, this can be configured one of four ways:
+ *
+ * - `append` - appends the result to the container
+ * - `prepend` - prepends the result to the container
+ * - `replace` - replaces the contents of the container
+ * - `remove` - removes the container
+ *
+ * @customElement turbo-stream
+ * @example
+ *   <turbo-stream action="append" target="dom_id">
+ *     <template>
+ *       Content to append to container designated with the dom_id.
+ *     </template>
+ *   </turbo-stream>
+ */
 export class StreamElement extends HTMLElement {
   async connectedCallback() {
     try {
@@ -29,10 +47,16 @@ export class StreamElement extends HTMLElement {
     try { this.remove() } catch {}
   }
  
+  /**
+   * Removes duplicate children (by ID)
+   */
   removeDuplicateTargetChildren() {
     this.duplicateChildren.forEach(c => c.remove())
   }
   
+  /**
+   * Gets the list of duplicate children (i.e. those with the same ID)
+   */
   get duplicateChildren() {
     const existingChildren = this.targetElements.flatMap(e => [...e.children]).filter(c => !!c.id)
     const newChildrenIds   = [...this.templateContent?.children].filter(c => !!c.id).map(c => c.id)
@@ -40,6 +64,10 @@ export class StreamElement extends HTMLElement {
     return existingChildren.filter(c => newChildrenIds.includes(c.id))
   }
   
+
+  /**
+   * Gets the action function to be performed.
+   */
   get performAction() {
     if (this.action) {
       const actionFunction = StreamActions[this.action]
@@ -51,6 +79,9 @@ export class StreamElement extends HTMLElement {
     this.raise("action attribute is missing")
   }
 
+  /**
+   * Gets the target elements which the template will be rendered to.
+   */
   get targetElements() {
     if (this.target) { 
       return this.targetElementsById
@@ -61,10 +92,16 @@ export class StreamElement extends HTMLElement {
     }
   }
 
+  /**
+   * Gets the contents of the main `<template>`.
+   */
   get templateContent() {
     return this.templateElement.content.cloneNode(true)
   }
 
+  /**
+   * Gets the main `<template>` used for rendering
+   */
   get templateElement() {
     if (this.firstElementChild instanceof HTMLTemplateElement) {
       return this.firstElementChild
@@ -72,14 +109,24 @@ export class StreamElement extends HTMLElement {
     this.raise("first child element must be a <template> element")
   }
 
+  /**
+   * Gets the current action.
+   */
   get action() {
     return this.getAttribute("action")
   }
 
+  /**
+   * Gets the current target (an element ID) to which the result will
+   * be rendered.
+   */
   get target() {
     return this.getAttribute("target")
   }
 
+  /**
+   * Gets the current "targets" selector (a CSS selector)
+   */
   get targets() {
     return this.getAttribute("targets")
   }
