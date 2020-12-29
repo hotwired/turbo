bc0da0fb861eb85c4ea3ae16c15ef8752abc39ba
Sean Doyle <sean.p.doyle24@gmail.com>
2020-12-29 09:28:40 -0500

    dev-builds/85f46ed Support Lazy-loading `<turbo-frame>` contents
    https://github.com/hotwired/turbo/tree/85f46edc3b61c8a27035dde35f78785363660cbc

---
+++ dist/types/elements/frame_element.d.ts
@@ -0,0 +1,26 @@
+import { FetchResponse } from "../http/fetch_response";
+import { FrameController } from "../core/frames/frame_controller";
+export declare enum FrameLoadingStyle {
+    eager = 0,
+    lazy = 1
+}
+export declare class FrameElement extends HTMLElement {
+    readonly controller: FrameController;
+    static get observedAttributes(): string[];
+    constructor();
+    connectedCallback(): void;
+    disconnectedCallback(): void;
+    attributeChangedCallback(): void;
+    formSubmissionIntercepted(element: HTMLFormElement, submitter?: HTMLElement): void;
+    get src(): string | null;
+    set src(value: string | null);
+    get loading(): FrameLoadingStyle;
+    set loading(value: FrameLoadingStyle);
+    get loaded(): Promise<FetchResponse | undefined>;
+    get disabled(): boolean;
+    set disabled(value: boolean);
+    get autoscroll(): boolean;
+    set autoscroll(value: boolean);
+    get isActive(): boolean;
+    get isPreview(): boolean;
+}
