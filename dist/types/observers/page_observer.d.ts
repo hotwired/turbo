760791cb3964304efcc25bc751f5bc82273b864e
Javan Makhmali <javan@javan.us>
2020-12-30 09:41:58 -0500

    dev-builds/66dda1a Remove unused workflow file
    https://github.com/hotwired/turbo/tree/66dda1a3eb3734a519e9f504172954466a019c34

---
+++ dist/types/observers/page_observer.d.ts
@@ -0,0 +1,25 @@
+export interface PageObserverDelegate {
+    pageBecameInteractive(): void;
+    pageLoaded(): void;
+    pageInvalidated(): void;
+}
+export declare enum PageStage {
+    initial = 0,
+    loading = 1,
+    interactive = 2,
+    complete = 3,
+    invalidated = 4
+}
+export declare class PageObserver {
+    readonly delegate: PageObserverDelegate;
+    stage: PageStage;
+    started: boolean;
+    constructor(delegate: PageObserverDelegate);
+    start(): void;
+    stop(): void;
+    invalidate(): void;
+    interpretReadyState: () => void;
+    pageIsInteractive(): void;
+    pageIsComplete(): void;
+    get readyState(): DocumentReadyState;
+}
