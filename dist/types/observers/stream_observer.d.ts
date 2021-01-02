eccf68fa915bb994e61d875d6c2b24816f74bcec
Sean Doyle <sean.p.doyle24@gmail.com>
2021-01-02 18:22:59 -0500

    dev-builds/7dd1282 move Accept header to FormSubmission
    https://github.com/hotwired/turbo/tree/7dd12828fc546b4cf4a0e6f9b8e01fa814b4a613

---
+++ dist/types/observers/stream_observer.d.ts
@@ -0,0 +1,20 @@
+import { FetchResponse } from "../http/fetch_response";
+import { StreamMessage } from "../core/streams/stream_message";
+import { StreamSource } from "../core/types";
+export interface StreamObserverDelegate {
+    receivedMessageFromStream(message: StreamMessage): void;
+}
+export declare class StreamObserver {
+    readonly delegate: StreamObserverDelegate;
+    readonly sources: Set<StreamSource>;
+    private started;
+    constructor(delegate: StreamObserverDelegate);
+    start(): void;
+    stop(): void;
+    connectStreamSource(source: StreamSource): void;
+    disconnectStreamSource(source: StreamSource): void;
+    streamSourceIsConnected(source: StreamSource): boolean;
+    receiveMessageEvent: (event: MessageEvent) => void;
+    receiveMessageResponse(response: FetchResponse): Promise<void>;
+    receiveMessageHTML(html: string): void;
+}
