bd2a9a6c7d8f3759d0a80ba5d1f9e76bc62157bf
Sean Doyle <sean.p.doyle24@gmail.com>
2021-01-03 11:25:54 -0500

    dev-builds/b32311e Add test coverage for turbo-frame[disabled]
    https://github.com/hotwired/turbo/tree/b32311eed0ddd1c26ae50aec4799933a673ff43f

---
+++ dist/types/tests/functional/form_submission_tests.d.ts
@@ -0,0 +1,19 @@
+import { TurboDriveTestCase } from "../helpers/turbo_drive_test_case";
+export declare class FormSubmissionTests extends TurboDriveTestCase {
+    setup(): Promise<void>;
+    "test standard form submission with redirect response"(): Promise<void>;
+    "test invalid form submission with unprocessable entity status"(): Promise<void>;
+    "test invalid form submission with server error status"(): Promise<void>;
+    "test submitter form submission reads button attributes"(): Promise<void>;
+    "test frame form submission with redirect response"(): Promise<void>;
+    "test invalid frame form submission with unprocessable entity status"(): Promise<void>;
+    "test invalid frame form submission with internal server errror status"(): Promise<void>;
+    "test frame form submission with stream response"(): Promise<void>;
+    "test form submission with [data-turbo=false] on the form"(): Promise<void>;
+    "test form submission with [data-turbo=false] on the submitter"(): Promise<void>;
+    "test form submission skipped within method=dialog"(): Promise<void>;
+    "test form submission skipped with submitter formmethod=dialog"(): Promise<void>;
+    "test form submission targets disabled frame"(): Promise<void>;
+    listenForFormSubmissions(): void;
+    get turboFormSubmitted(): Promise<boolean>;
+}
