e741a91f8fc4273ce8742fd90ef5c6045d98ed13
Sam Stephenson <sam@hey.com>
2021-01-08 10:50:22 -0600

    dev-builds/5525c21 Scope `history.scrollRestoration` more tightly
    https://github.com/hotwired/turbo/tree/5525c21288edc222de0a05c72538aa7f898abd0c

---
+++ dist/types/tests/functional/scroll_restoration_tests.d.ts
@@ -0,0 +1,6 @@
+import { TurboDriveTestCase } from "../helpers/turbo_drive_test_case";
+export declare class ScrollRestorationTests extends TurboDriveTestCase {
+    "test landing on an anchor"(): Promise<void>;
+    "test reloading after scrolling"(): Promise<void>;
+    "test returning from history"(): Promise<void>;
+}
