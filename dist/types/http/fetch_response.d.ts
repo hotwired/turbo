6eab3fbfbbf399c1822075defe61f35d305b7a94
Sam Stephenson <sam@hey.com>
2021-01-10 20:33:14 -0600

    dev-builds/429f0fd Track the loading URL; expose FrameElement#complete
    https://github.com/hotwired/turbo/tree/429f0fde45b9bb10fe14e8a4ba8f7c5bdf9f5198

---
+++ dist/types/http/fetch_response.d.ts
@@ -0,0 +1,17 @@
+import { Location } from "../core/location";
+export declare class FetchResponse {
+    readonly response: Response;
+    constructor(response: Response);
+    get succeeded(): boolean;
+    get failed(): boolean;
+    get clientError(): boolean;
+    get serverError(): boolean;
+    get redirected(): boolean;
+    get location(): Location;
+    get isHTML(): "" | RegExpMatchArray | null;
+    get statusCode(): number;
+    get contentType(): string | null;
+    get responseText(): Promise<string>;
+    get responseHTML(): Promise<string | undefined>;
+    header(name: string): string | null;
+}
