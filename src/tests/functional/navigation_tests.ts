e8574615ddfd7f9fec8ffd570f644cb8ab87251c
Sean Doyle <sean.p.doyle24@gmail.com>
2021-04-04 23:48:46 -0400

    Support `data-turbo-action="..."` on Form Submits
    The `Navigator` class translates `<form method="get">` submissions into
    `proposeVisit` calls, skipping the rest of the `FormSubmission`
    architecture.
    
    This commit adds support for declaring `[data-turbo-action="replace"]`
    (or `"restore"` or `"advance"`) as a [Visit Action][]. Prior to this
    commit, specifying a `Visit` action was limited to an `<a>`
    click-initiated `Visit` only.
    
    This can be useful for a `GET` form driven by auto-submission driving
    the current page's URL, like a Typeahead autocomplete `<input
    type="search">` field or a bounding-box scrolling Map. In those
    scenarios, a series of `"advance"` page visits would yield an
    essentially broken "Back" and "Forward" navigation experience.
    
    [Visit Action]: https://turbo.hotwire.dev/handbook/drive#application-visits

--- src/tests/functional/navigation_tests.ts
+++ src/tests/functional/navigation_tests.ts
@@ -17,8 +17,29 @@ export class NavigationTests extends TurboDriveTestCase {
     this.assert.equal(await this.visitAction, "advance")
   }
 
+  async "test following a same-origin unannotated form[method=GET]"() {
+    this.clickSelector("#same-origin-unannotated-form button")
+    await this.nextBody
+    this.assert.equal(await this.pathname, "/src/tests/fixtures/one.html")
+    this.assert.equal(await this.visitAction, "advance")
+  }
+
   async "test following a same-origin data-turbo-action=replace link"() {
     this.clickSelector("#same-origin-replace-link")
+    await this.nextBody
+    this.assert.equal(await this.pathname, "/src/tests/fixtures/one.html")
+    this.assert.equal(await this.visitAction, "replace")
+  }
+
+  async "test following a same-origin data-turbo-action=replace form[method=GET]"() {
+    this.clickSelector("#same-origin-replace-form button")
+    await this.nextBody
+    this.assert.equal(await this.pathname, "/src/tests/fixtures/one.html")
+    this.assert.equal(await this.visitAction, "replace")
+  }
+
+  async "test following a same-origin form with button[data-turbo-action=replace]"() {
+    this.clickSelector("#same-origin-replace-form-submitter button")
     await this.nextBody
     this.assert.equal(await this.pathname, "/src/tests/fixtures/one.html")
     this.assert.equal(await this.visitAction, "replace")
