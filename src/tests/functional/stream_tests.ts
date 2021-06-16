236dd7601cb991a5991d858956f97942c14072dd
David Heinemeier Hansson <david@loudthinking.com>
2021-06-16 19:59:42 +0200

    Use form in HTML

--- src/tests/functional/stream_tests.ts
+++ src/tests/functional/stream_tests.ts
@@ -27,22 +27,12 @@ export class StreamTests extends FunctionalTestCase {
     this.assert.equal(await element[0].getVisibleText(), "Second")
     this.assert.equal(await element[1].getVisibleText(), "Third")
 
-    await this.createMessage("Hello CSS!", ".messages")
+    await this.clickSelector("#replace [type=submit]")
     await this.nextBeat
 
     element = await this.querySelectorAll(selector)
     this.assert.equal(await element[0].getVisibleText(), "Hello CSS!")
     this.assert.equal(await element[1].getVisibleText(), "Hello CSS!")
-  }
-  
-  async createMessage(content: string, target?: string) {
-    return this.post("/__turbo/messages", { content , target})
-  }
-
-  async post(path: string, params: any = {}) {
-    await this.evaluate((path, method, params) => {
-      fetch(location.origin + path, { method, body: new URLSearchParams(params) })
-    }, path, "POST", params)
   }
 }
 
