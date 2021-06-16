fa4135ca5ce8483481019401335fbea7d5401ec7
David Heinemeier Hansson <david@loudthinking.com>
2021-06-16 18:31:58 +0200

    Bad merge

--- src/elements/stream_element.ts
+++ src/elements/stream_element.ts
@@ -36,8 +36,6 @@ export class StreamElement extends HTMLElement {
   }
 
   get duplicateChildren() {
-    return [...this.templateContent?.children].filter(templateChild => !!templateChild.id).map(templateChild => {
-      let targetChild = [...this.targetElements.flatMap(e => e.children)].filter(c => c.id === templateChild.id)[0]
     const existingChildren = this.targetElements.flatMap(e => [...e.children]).filter(c => !!c.id)
     const newChildren      = [...this.templateContent?.children].filter(c => !!c.id)
 
