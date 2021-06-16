9a475320e59e455cc79c69e270bbf7162ede578b
David Heinemeier Hansson <david@loudthinking.com>
2021-06-16 19:17:50 +0200

    targetElements will always return an array

--- src/core/streams/stream_actions.ts
+++ src/core/streams/stream_actions.ts
@@ -2,35 +2,33 @@ import { StreamElement } from "../../elements/stream_element"
 
 export const StreamActions: { [action: string]: (this: StreamElement) => void } = {
   after() {
-    this.targetElements?.forEach(e => e.parentElement?.insertBefore(this.templateContent, e.nextSibling))
+    this.targetElements.forEach(e => e.parentElement?.insertBefore(this.templateContent, e.nextSibling))
   },
 
   append() {
     this.removeDuplicateTargetChildren()
-    this.targetElements?.forEach(e => e.append(this.templateContent))
+    this.targetElements.forEach(e => e.append(this.templateContent))
   },
 
   before() {
-    this.targetElements?.forEach(e => e.parentElement?.insertBefore(this.templateContent, e))
+    this.targetElements.forEach(e => e.parentElement?.insertBefore(this.templateContent, e))
   },
 
   prepend() {
     this.removeDuplicateTargetChildren()
-    this.targetElements?.forEach(e => e.prepend(this.templateContent))
+    this.targetElements.forEach(e => e.prepend(this.templateContent))
   },
 
   remove() {
-    this.targetElements?.forEach(e => e.remove())
+    this.targetElements.forEach(e => e.remove())
   },
 
   replace() {
-    this.targetElements?.forEach(e => e.replaceWith(this.templateContent))
+    this.targetElements.forEach(e => e.replaceWith(this.templateContent))
   },
 
   update() {
-    if (this.targetElements) {
-      this.targetElements.forEach(e => e.innerHTML = "")
-      this.targetElements.forEach(e => e.append(this.templateContent))
-    }
+    this.targetElements.forEach(e => e.innerHTML = "")
+    this.targetElements.forEach(e => e.append(this.templateContent))
   }
 }
