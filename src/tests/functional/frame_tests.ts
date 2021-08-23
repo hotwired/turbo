bc5b7cd6c21f253b85415535d3087fe06e0937fb
Kirill Platonov <mail@kirillplatonov.com>
2021-08-23 12:51:12 +0300

    Reset reloadable frame on reconnect and for form submissions (#350)

--- src/tests/functional/frame_tests.ts
+++ src/tests/functional/frame_tests.ts
@@ -160,6 +160,28 @@ export class FrameTests extends TurboDriveTestCase {
     await this.nextEventNamed("turbo:before-fetch-request")
   }
 
+  async "test reconnecting after following a link does not reload the frame"() {
+    await this.clickSelector("#hello a")
+    await this.nextEventNamed("turbo:before-fetch-request")
+
+    await this.remote.execute(() => {
+      window.savedElement = document.querySelector("#frame")
+      window.savedElement?.remove()
+    })
+    await this.nextBeat
+
+    await this.remote.execute(() => {
+      if (window.savedElement) {
+        document.body.appendChild(window.savedElement)
+      }
+    })
+    await this.nextBeat
+
+    const eventLogs = await this.eventLogChannel.read()
+    const requestLogs = eventLogs.filter(([name]) => name == "turbo:before-fetch-request")
+    this.assert.equal(requestLogs.length, 0)
+  }
+
   get frameScriptEvaluationCount(): Promise<number | undefined> {
     return this.evaluate(() => window.frameScriptEvaluationCount)
   }
