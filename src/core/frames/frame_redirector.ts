11840c1ba45cc82e59830b23353db140fcd768c5
Sean Doyle <seanpdoyle@users.noreply.github.com>
2021-09-10 05:58:27 -0400

    Read `data-turbo-frame` target from Submitter (#381)
    * Read `data-turbo-frame` target from Submitter
    
    Adds support for reading the value of `data-turbo-frame` from a
    `submitter` (transmitted as part of the `SubmitEvent.submitter`
    property) as if it were a `<button>` element's [formtarget][] attribute.
    
    This change comes with an added benefit of meshing with Rails'
    [button_to][] helper:
    
    ```ruby
    button_to "Submit to turbo-frame#frame_id",
      form: { data: { turbo_frame: "frame_id" } } # before
    
    button_to "Submit to  turbo-frame#frame_id",
      data: { turbo_frame: "frame_id" }           # after
    ```
    
    [formtarget]: https://developer.mozilla.org/en-US/docs/Web/API/HTMLButtonElement
    [button_to]: https://edgeapi.rubyonrails.org/classes/ActionView/Helpers/UrlHelper.html#method-i-button_to
    
    * Err from merge
    
    Co-authored-by: David Heinemeier Hansson <david@loudthinking.com>

--- src/core/frames/frame_redirector.ts
+++ src/core/frames/frame_redirector.ts
@@ -40,7 +40,7 @@ export class FrameRedirector implements LinkInterceptorDelegate, FormInterceptor
   }
 
   formSubmissionIntercepted(element: HTMLFormElement, submitter?: HTMLElement) {
-    const frame = this.findFrameElement(element)
+    const frame = this.findFrameElement(element, submitter)
     if (frame) {
       frame.removeAttribute("reloadable")
       frame.delegate.formSubmissionIntercepted(element, submitter)
@@ -48,12 +48,12 @@ export class FrameRedirector implements LinkInterceptorDelegate, FormInterceptor
   }
 
   private shouldRedirect(element: Element, submitter?: HTMLElement) {
-    const frame = this.findFrameElement(element)
+    const frame = this.findFrameElement(element, submitter)
     return frame ? frame != element.closest("turbo-frame") : false
   }
 
-  private findFrameElement(element: Element) {
-    const id = element.getAttribute("data-turbo-frame")
+  private findFrameElement(element: Element, submitter?: HTMLElement) {
+    const id = submitter?.getAttribute("data-turbo-frame") || element.getAttribute("data-turbo-frame")
     if (id && id != "_top") {
       const frame = this.element.querySelector(`#${id}:not([disabled])`)
       if (frame instanceof FrameElement) {
