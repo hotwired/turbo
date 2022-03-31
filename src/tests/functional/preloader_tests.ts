08646ac952eac98d945029b4adf3c44d23d7efbc
Leon Pearce <leonp1991@gmail.com>
2022-03-31 16:40:20 -0500

    (fix) hopefully test suite is now green

--- src/tests/functional/preloader_tests.ts
+++ src/tests/functional/preloader_tests.ts
@@ -1,12 +1,14 @@
 import { TurboDriveTestCase } from "../helpers/turbo_drive_test_case"
 
+const preloadedUrl = "http://localhost:9000/src/tests/fixtures/preloaded.html"
+
 export class PreloaderTests extends TurboDriveTestCase {
   async "test preloads snapshot on initial load"() {
     // contains `a[rel="preload"][href="http://localhost:9000/src/tests/fixtures/preloaded.html"]`
     await this.goToLocation("/src/tests/fixtures/preloading.html")
 
     this.assert.ok(await this.remote.execute(() =>
-      "http://localhost:9000/src/tests/fixtures/preloaded.html" in window.Turbo.session.preloader.snapshotCache.snapshots
+       preloadedUrl in window.Turbo.session.preloader.snapshotCache.snapshots
     ))
   }
 
@@ -16,18 +18,21 @@ export class PreloaderTests extends TurboDriveTestCase {
 
     // contains `a[rel="preload"][href="http://localhost:9000/src/tests/fixtures/preloaded.html"]`
     await this.clickSelector("#hot_preload_anchor")
+    await this.waitUntilSelector("#preload_anchor")
 
     this.assert.ok(await this.remote.execute(() =>
-      "http://localhost:9000/src/tests/fixtures/preloaded.html" in window.Turbo.session.preloader.snapshotCache.snapshots
+       preloadedUrl in window.Turbo.session.preloader.snapshotCache.snapshots
     ))
   }
 
-  async "test navigates to preloaded snapshot from eager frame"() {
+  async "test navigates to preloaded snapshot from frame"() {
     // contains `a[rel="preload"][href="http://localhost:9000/src/tests/fixtures/preloaded.html"]`
-    await this.goToLocation("/src/tests/fixtures/preloading.html")
+    await this.goToLocation("/src/tests/fixtures/frame_preloading.html")
+    await this.waitUntilSelector("#frame_preload_anchor")
+    await this.nextBeat
 
     this.assert.ok(await this.remote.execute(() =>
-      "http://localhost:9000/src/tests/fixtures/preloaded.html" in window.Turbo.session.preloader.snapshotCache.snapshots
+       preloadedUrl in window.Turbo.session.preloader.snapshotCache.snapshots
     ))
   }
 }
