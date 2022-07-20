ecdd1c9a054914f19e69f013c94941f46a4c423a
Simon Taranto <srt32@users.noreply.github.com>
2022-07-20 14:05:56 +0000

    Add spec

--- src/tests/functional/frame_navigation_tests.ts
+++ src/tests/functional/frame_navigation_tests.ts
@@ -1,4 +1,5 @@
 import { test } from "@playwright/test"
+import { chromium, firefox } from "@playwright/test"
 import { getFromLocalStorage, nextEventNamed, nextEventOnTarget, pathname } from "../helpers/page"
 import { assert } from "chai"
 
@@ -22,6 +23,24 @@ test("test frame navigation with exterior link", async ({ page }) => {
   await page.click("#outside")
 
   await nextEventOnTarget(page, "frame", "turbo:frame-load")
+})
+
+test("test frame navigation emits fetch-error event when offline", async ({ page }) => {
+  let browser
+  if (process.env.browserName === "chromium") {
+    browser = await chromium.launch()
+  } else if (process.env.browserName === "firefox") {
+    browser = await firefox.launch()
+  } else {
+    throw new Error("unsupported browser")
+  }
+  const context = await browser.newContext()
+  page = await context.newPage()
+  await context.setOffline(true)
+  await page.goto("/src/tests/fixtures/tabs.html")
+  await page.click("#tab-2")
+  await nextEventOnTarget(page, "frame", "turbo:fetch-error")
+  await context.close()
 })
 
 test("test promoted frame navigation updates the URL before rendering", async ({ page }) => {
