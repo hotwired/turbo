e055bdfc31492b01da32f64439a91f41aeb184ae
Sean Doyle <sean.p.doyle24@gmail.com>
2022-08-01 15:37:47 -0400

    Resolve flaky loading test
    Replace sequentially waiting for `turbo:frame-load` events in a certain
    order, change the test to be particular about _which_ frame the events
    should be firing on (in this case, the `turbo-frame[id="hello"]`
    element).
    
    ```
    1) [firefox] › loading_tests.ts:140:1 › test changing [src] attribute on a [complete] frame with loading=lazy defers navigation
    
      Timeout of 30000ms exceeded.
    
      page.evaluate: Target closed
    
        139 |
        140 | test("test changing [src] attribute on a [complete] frame with loading=lazy defers navigation", async ({ page }) => {
      > 141 |   await nextEventOnTarget(page, "frame", "turbo:frame-load")
            |   ^
        142 |   await page.click("#loading-lazy summary")
        143 |   await nextEventOnTarget(page, "hello", "turbo:frame-load")
        144 |
    
          at readArray (/home/runner/work/turbo/turbo/src/tests/helpers/page.ts:127:15)
          at readEventLogs (/home/runner/work/turbo/turbo/src/tests/helpers/page.ts:141:10)
          at nextEventOnTarget (/home/runner/work/turbo/turbo/src/tests/helpers/page.ts:81:27)
          at /home/runner/work/turbo/turbo/src/tests/functional/loading_tests.ts:141:3
    
      Pending operations:
        - page.evaluate at src/tests/helpers/page.ts:127:15
    ```

--- src/tests/functional/loading_tests.ts
+++ src/tests/functional/loading_tests.ts
@@ -7,7 +7,7 @@ import {
   nextBody,
   nextEventNamed,
   nextEventOnTarget,
-  noNextEventNamed,
+  noNextEventOnTarget,
   readEventLogs,
 } from "../helpers/page"
 
@@ -138,7 +138,6 @@ test("test removing the [complete] attribute of an eager frame reloads the conte
 })
 
 test("test changing [src] attribute on a [complete] frame with loading=lazy defers navigation", async ({ page }) => {
-  await nextEventOnTarget(page, "frame", "turbo:frame-load")
   await page.click("#loading-lazy summary")
   await nextEventOnTarget(page, "hello", "turbo:frame-load")
 
@@ -150,7 +149,7 @@ test("test changing [src] attribute on a [complete] frame with loading=lazy defe
   await nextEventNamed(page, "turbo:load")
   await page.goBack()
   await nextEventNamed(page, "turbo:load")
-  await noNextEventNamed(page, "turbo:frame-load")
+  await noNextEventOnTarget(page, "hello", "turbo:frame-load")
 
   let src = new URL((await attributeForSelector(page, "#hello", "src")) || "")
 
@@ -158,7 +157,7 @@ test("test changing [src] attribute on a [complete] frame with loading=lazy defe
   assert.equal(src.pathname, "/src/tests/fixtures/frames/hello.html", "lazy frame retains [src]")
 
   await page.click("#link-lazy-frame")
-  await noNextEventNamed(page, "turbo:frame-load")
+  await noNextEventOnTarget(page, "hello", "turbo:frame-load")
 
   assert.ok(await hasSelector(page, "#loading-lazy turbo-frame:not([complete])"), "lazy frame is not complete")
 
