f72a40c72401badaebb4402742275a445eb9dbdc
Sean Doyle <seanpdoyle@users.noreply.github.com>
2022-08-05 10:12:01 -0400

    Preserve input values in cache (#666)
    Re-submission of https://github.com/hotwired/turbo/pull/70
    
    This change will save the values of `<input>`s, `<textarea>`s, and
    `<select>`s when creating a snapshot so that when you navigate back or
    forward in history, form fields will not be reset.
    
    This behavior was already present prior to [5dfc79e][] for `<input>`s
    and `<textarea>`s but not `<select>`s (see
    [turbolinks/turbolinks#238][]).
    
    [5dfc79e]: https://github.com/hotwired/turbo/commit/5dfc79ee2feba441ea9c9b486009d08336e78d76
    [turbolinks/turbolinks#238]: https://github.com/turbolinks/turbolinks/issues/238
    
    Co-authored-by: Jeremiah <jeremiah@entretechno.com>

--- src/core/drive/page_snapshot.ts
+++ src/core/drive/page_snapshot.ts
@@ -24,7 +24,22 @@ export class PageSnapshot extends Snapshot<HTMLBodyElement> {
   }
 
   clone() {
-    return new PageSnapshot(this.element.cloneNode(true), this.headSnapshot)
+    const clonedElement = this.element.cloneNode(true)
+
+    const selectElements = this.element.querySelectorAll("select")
+    const clonedSelectElements = clonedElement.querySelectorAll("select")
+
+    for (const [index, source] of selectElements.entries()) {
+      for (const [optionIndex, option] of Array.from(source.options).entries()) {
+        clonedSelectElements[index].options[optionIndex].selected = option.selected
+      }
+    }
+
+    for (const clonedPasswordInput of clonedElement.querySelectorAll<HTMLInputElement>('input[type="password"]')) {
+      clonedPasswordInput.value = ""
+    }
+
+    return new PageSnapshot(clonedElement, this.headSnapshot)
   }
 
   get headElement() {
