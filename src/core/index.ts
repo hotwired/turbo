1500d446029e06a0892bef5a0acc44c6eaad95f2
Sean Doyle <sean.p.doyle24@gmail.com>
2022-09-14 13:45:32 -0400

    Add test coverage for exports
    Introduce the `src/tests/unit/export_tests.ts` module to cover what's
    exported from the `src/index.ts` module, including both Turbo-prefixed
    classes and objects and functions accessible as the `Turbo.`-prefixed
    accessors.

--- src/core/index.ts
+++ src/core/index.ts
@@ -19,6 +19,7 @@ export {
   TurboBeforeRenderEvent,
   TurboBeforeVisitEvent,
   TurboClickEvent,
+  TurboBeforeFrameRenderEvent,
   TurboFrameLoadEvent,
   TurboFrameRenderEvent,
   TurboLoadEvent,
--- src/tests/unit/index.ts
+++ src/tests/unit/index.ts
@@ -1,2 +1,3 @@
+export * from "./export_tests"
 export * from "./deprecated_adapter_support_test"
 export * from "./stream_element_tests"
