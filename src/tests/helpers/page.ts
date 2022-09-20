6a0167f2fee603754ba05e2a38f3b884c45df0c3
Sean Doyle <sean.p.doyle24@gmail.com>
2022-09-20 13:46:09 -0400

    Don't toggle `turbo-frame[busy]` when navigating `_top`
    Closes https://github.com/hotwired/turbo/issues/727
    
    Add test coverage to ensure that an `<a>` element click or `<form>`
    element submission nested within a `<turbo-frame>` _does not_ toggle
    `[aria-busy]` or `[busy]` on the `turbo-frame` if the navigation is
    targeting `_top` (with `[data-turbo-frame="_top"]`).

--- src/tests/helpers/page.ts
+++ src/tests/helpers/page.ts
@@ -111,7 +111,7 @@ export async function noNextAttributeMutationNamed(
   attributeName: string
 ): Promise<boolean> {
   const records = await readMutationLogs(page, 1)
-  return !records.some(([name]) => name == attributeName)
+  return !records.some(([name, _, target]) => name == attributeName && target == elementId)
 }
 
 export async function noNextEventNamed(page: Page, eventName: string): Promise<boolean> {
