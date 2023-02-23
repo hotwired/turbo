455ffe0e83677838ad642c0ae11fb7d90a28ff4e
Alberto Fernández-Capel <alberto@hey.com>
2023-02-23 12:01:35 +0000

    Allow changing the submitter text during form submission (#869)
    This change introduces a new optional data-turbo-submits-with text
    attribute that can be set on submit elements (inputs or buttons) in
    Turbo forms.
    
          <form action="/" method="post">
            <input type="submit" value="Save" data-turbo-submits-with="Saving...">
          </form>
    
    When the form is in a submitting state Turbo will change the submitter
    content -the input value for inputs, and the innerHTML for buttons- with
    the data-turbo-submits-with value, and restore the original value when the
    submission ends.
    
    This is a common requirement in many apps, to style the form submitting
    state and give feedback to the user that a click is already being
    processed.

--- src/tests/functional/form_submission_tests.ts
+++ src/tests/functional/form_submission_tests.ts
@@ -175,6 +175,40 @@ test("test standard POST form submission toggles submitter [disabled] attribute"
   )
 })
 
+test("replaces input value with data-turbo-submits-with on form submission", async ({ page }) => {
+  page.click("#submits-with-form-input")
+
+  assert.equal(
+    await nextAttributeMutationNamed(page, "submits-with-form-input", "value"),
+    "Saving...",
+    "sets data-turbo-submits-with on the submitter"
+  )
+
+  assert.equal(
+    await nextAttributeMutationNamed(page, "submits-with-form-input", "value"),
+    "Save",
+    "restores the original submitter text value"
+  )
+})
+
+test("replaces button innerHTML with data-turbo-submits-with on form submission", async ({ page }) => {
+  await page.click("#submits-with-form-button")
+
+  await nextEventNamed(page, "turbo:submit-start")
+  assert.equal(
+    await page.textContent("#submits-with-form-button"),
+    "Saving...",
+    "sets data-turbo-submits-with on the submitter"
+  )
+
+  await nextEventNamed(page, "turbo:submit-end")
+  assert.equal(
+    await page.textContent("#submits-with-form-button"),
+    "Save",
+    "sets data-turbo-submits-with on the submitter"
+  )
+})
+
 test("test standard GET form submission", async ({ page }) => {
   await page.click("#standard form.greeting input[type=submit]")
   await nextBody(page)
