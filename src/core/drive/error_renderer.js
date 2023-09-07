d494994c6a2f939751b606fe2df777b19dc72877
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2023-09-07 19:01:49 +0100

    Refactor ErrorRenderer class for better modularity and maintainability
    - Pass 'document' and 'newSnapshot' as constructor parameters to reduce global variable reliance.
    - Use constants for import paths to improve code readability.
    - Split 'render' method into 'replaceHeadAndBody' and 'activateScriptElements' for better organization.

--- src/core/drive/error_renderer.js
+++ src/core/drive/error_renderer.js
@@ -1,11 +1,17 @@
 import { activateScriptElement } from "../../util"
 import { Renderer } from "../renderer"
 
-export class ErrorRenderer extends Renderer {
-  static renderElement(currentElement, newElement) {
-    const { documentElement, body } = document
+const SELECTORS = {
+  SCRIPT_ELEMENTS: "script"
+}
 
-    documentElement.replaceChild(newElement, body)
+export class ErrorRenderer extends Renderer {
+  constructor(newElement, currentElement, document, newSnapshot) {
+    super()
+    this.newElement = newElement
+    this.currentElement = currentElement
+    this.document = document
+    this.newSnapshot = newSnapshot
   }
 
   async render() {
@@ -14,26 +20,31 @@ export class ErrorRenderer extends Renderer {
   }
 
   replaceHeadAndBody() {
-    const { documentElement, head } = document
+    const { documentElement, head } = this.document
     documentElement.replaceChild(this.newHead, head)
+    this.replaceBody()
+  }
+
+  replaceBody() {
     this.renderElement(this.currentElement, this.newElement)
   }
 
   activateScriptElements() {
-    for (const replaceableElement of this.scriptElements) {
+    const scriptElements = this.getScriptElements()
+    scriptElements.forEach((replaceableElement) => {
       const parentNode = replaceableElement.parentNode
       if (parentNode) {
         const element = activateScriptElement(replaceableElement)
         parentNode.replaceChild(element, replaceableElement)
       }
-    }
+    })
   }
 
   get newHead() {
     return this.newSnapshot.headSnapshot.element
   }
 
-  get scriptElements() {
-    return document.documentElement.querySelectorAll("script")
+  getScriptElements() {
+    return this.document.documentElement.querySelectorAll(SELECTORS.SCRIPT_ELEMENTS)
   }
 }
