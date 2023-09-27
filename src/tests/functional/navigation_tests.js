hotwired/turbo

commit f86a376b0d088c8ff83ef8180b95d492527902e5

Add disk cache store (#949)
This commit extends the Turbo cache API to allow for the use of
different cache stores. The default store is still the in-memory store,
but a new persistent cache store is now available.

The disk store uses the [Cache API](https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage)
to store and retrieve snapshots from the browser's cache. This allows
for the snapshots to be persisted across different tabs, page reloads
and even browser restarts.

The disk store is not enabled by default. To enable it, you need to set
the `Turbo.cache.store` property to `"disk"`.

```js
Turbo.cache.store = "disk"
```

This is also a stepping stone to implement offline support with Service
Workers. With a Service Worker in place, and the disk cache store
enabled, we can still serve cached snapshots even when the browser is
offline.

src/core/drive/visit.js
@@ -215,8 +215,8 @@ export class Visit {
     }
   }
 
-  getCachedSnapshot() {
-    const snapshot = this.view.getCachedSnapshotForLocation(this.location) || this.getPreloadedSnapshot()
+  async getCachedSnapshot() {
+    const snapshot = (await this.view.getCachedSnapshotForLocation(this.location)) || this.getPreloadedSnapshot()
 
     if (snapshot && (!getAnchor(this.location) || snapshot.hasAnchor(getAnchor(this.location)))) {
       if (this.action == "restore" || snapshot.isPreviewable) {
@@ -235,8 +235,8 @@ export class Visit {
     return this.getCachedSnapshot() != null
   }
 
-  loadCachedSnapshot() {
-    const snapshot = this.getCachedSnapshot()
+  async loadCachedSnapshot() {
+    const snapshot = await this.getCachedSnapshot()
     if (snapshot) {
       const isPreview = this.shouldIssueRequest()
       this.render(async () => {

src/core/cache.js
@@ -1,4 +1,5 @@
 import { setMetaContent } from "../util"
+import { SnapshotCache } from "./drive/snapshot_cache"
 
 export class Cache {
   constructor(session) {
@@ -6,7 +7,7 @@ export class Cache {
   }
 
   clear() {
-    this.session.clearCache()
+    this.store.clear()
   }
 
   resetCacheControl() {
@@ -19,6 +20,18 @@ export class Cache {
 
   exemptPageFromPreview() {
     this.#setCacheControl("no-preview")
+  }
+
+  set store(store) {
+    if (typeof store === "string") {
+      SnapshotCache.setStore(store)
+    } else {
+      SnapshotCache.currentStore = store
+    }
+  }
+
+  get store() {
+    return SnapshotCache.currentStore
   }
 
   #setCacheControl(value) {

src/core/drive/cache_stores/disk_store.js
@@ -1,0 +1,64 @@
+import { PageSnapshot } from "../page_snapshot"
+
+export class DiskStore {
+  _version = "v1"
+
+  constructor() {
+    if (typeof caches === "undefined") {
+      throw new Error("windows.caches is undefined. CacheStore requires a secure context.")
+    }
+
+    this.storage = this.openStorage()
+  }
+
+  async has(location) {
+    const storage = await this.openStorage()
+    return (await storage.match(location)) !== undefined
+  }
+
+  async get(location) {
+    const storage = await this.openStorage()
+    const response = await storage.match(location)
+
+    if (response && response.ok) {
+      const html = await response.text()
+      return PageSnapshot.fromHTMLString(html)
+    }
+  }
+
+  async put(location, snapshot) {
+    const storage = await this.openStorage()
+
+    const response = new Response(snapshot.html, {
+      status: 200,
+      statusText: "OK",
+      headers: {
+        "Content-Type": "text/html"
+      }
+    })
+    await storage.put(location, response)
+    return snapshot
+  }
+
+  async clear() {
+    const storage = await this.openStorage()
+    const keys = await storage.keys()
+    await Promise.all(keys.map((key) => storage.delete(key)))
+  }
+
+  openStorage() {
+    this.storage ||= caches.open(`turbo-${this.version}`)
+    return this.storage
+  }
+
+  set version(value) {
+    if (value !== this._version) {
+      this._version = value
+      this.storage ||= caches.open(`turbo-${this.version}`)
+    }
+  }
+
+  get version() {
+    return this._version
+  }
+}

src/core/drive/cache_stores/memory_store.js
@@ -1,0 +1,56 @@
+import { toCacheKey } from "../../url"
+
+export class MemoryStore {
+  keys = []
+  snapshots = {}
+
+  constructor(size) {
+    this.size = size
+  }
+
+  async has(location) {
+    return toCacheKey(location) in this.snapshots
+  }
+
+  async get(location) {
+    if (await this.has(location)) {
+      const snapshot = this.read(location)
+      this.touch(location)
+      return snapshot
+    }
+  }
+
+  async put(location, snapshot) {
+    this.write(location, snapshot)
+    this.touch(location)
+    return snapshot
+  }
+
+  async clear() {
+    this.snapshots = {}
+  }
+
+  // Private
+
+  read(location) {
+    return this.snapshots[toCacheKey(location)]
+  }
+
+  write(location, snapshot) {
+    this.snapshots[toCacheKey(location)] = snapshot
+  }
+
+  touch(location) {
+    const key = toCacheKey(location)
+    const index = this.keys.indexOf(key)
+    if (index > -1) this.keys.splice(index, 1)
+    this.keys.unshift(key)
+    this.trim()
+  }
+
+  trim() {
+    for (const key of this.keys.splice(this.size)) {
+      delete this.snapshots[key]
+    }
+  }
+}

src/core/drive/page_snapshot.js
@@ -40,6 +40,10 @@ export class PageSnapshot extends Snapshot {
     return new PageSnapshot(clonedElement, this.headSnapshot)
   }
 
+  get html() {
+    return `${this.headElement.outerHTML}\n\n${this.element.outerHTML}`
+  }
+
   get headElement() {
     return this.headSnapshot.element
   }

src/core/drive/page_view.js
@@ -6,7 +6,7 @@ import { PageSnapshot } from "./page_snapshot"
 import { SnapshotCache } from "./snapshot_cache"
 
 export class PageView extends View {
-  snapshotCache = new SnapshotCache(10)
+  snapshotCache = new SnapshotCache()
   lastRenderedLocation = new URL(location.href)
   forceReloaded = false
 
@@ -30,6 +30,10 @@ export class PageView extends View {
     visit?.changeHistory()
     const renderer = new ErrorRenderer(this.snapshot, snapshot, ErrorRenderer.renderElement, false)
     return this.render(renderer)
+  }
+
+  setCacheStore(cacheName) {
+    SnapshotCache.setStore(cacheName)
   }
 
   clearSnapshotCache() {

src/core/drive/preloader.js
@@ -30,9 +30,7 @@ export class Preloader {
   async preloadURL(link) {
     const location = new URL(link.href)
 
-    if (this.snapshotCache.has(location)) {
-      return
-    }
+    if (await this.snapshotCache.has(location)) return
 
     try {
       const response = await fetch(location.toString(), { headers: { "Sec-Purpose": "prefetch", Accept: "text/html" } })

src/core/drive/snapshot_cache.js
@@ -1,56 +1,35 @@
-import { toCacheKey } from "../url"
+import { DiskStore } from "./cache_stores/disk_store"
+import { MemoryStore } from "./cache_stores/memory_store"
 
 export class SnapshotCache {
-  keys = []
-  snapshots = {}
+  static currentStore = new MemoryStore(10)
 
-  constructor(size) {
-    this.size = size
+  static setStore(storeName) {
+    switch (storeName) {
+      case "memory":
+        SnapshotCache.currentStore = new MemoryStore(10)
+        break
+      case "disk":
+        SnapshotCache.currentStore = new DiskStore()
+        break
+      default:
+        throw new Error(`Invalid store name: ${storeName}`)
+    }
   }
 
   has(location) {
-    return toCacheKey(location) in this.snapshots
+    return SnapshotCache.currentStore.has(location)
   }
 
   get(location) {
-    if (this.has(location)) {
-      const snapshot = this.read(location)
-      this.touch(location)
-      return snapshot
-    }
+    return SnapshotCache.currentStore.get(location)
   }
 
   put(location, snapshot) {
-    this.write(location, snapshot)
-    this.touch(location)
-    return snapshot
+    return SnapshotCache.currentStore.put(location, snapshot)
   }
 
   clear() {
-    this.snapshots = {}
-  }
-
-  // Private
-
-  read(location) {
-    return this.snapshots[toCacheKey(location)]
-  }
-
-  write(location, snapshot) {
-    this.snapshots[toCacheKey(location)] = snapshot
-  }
-
-  touch(location) {
-    const key = toCacheKey(location)
-    const index = this.keys.indexOf(key)
-    if (index > -1) this.keys.splice(index, 1)
-    this.keys.unshift(key)
-    this.trim()
-  }
-
-  trim() {
-    for (const key of this.keys.splice(this.size)) {
-      delete this.snapshots[key]
-    }
+    return SnapshotCache.currentStore.clear()
   }
 }

commit 4b292d4808e5e811cc39e98eeaaf99989d5ea28c

Fix back navigation after POST form submission
This commit fixes a bug where navigating back after submitting a POST
form would not show the previous page. The bug was introduced in
https://github.com/hotwired/turbo/pull/949. The new cache API is async
since it needs to access CacheStorage which is async. In that PR,
getCachedSnapshot was changed to be async, but hasCachedSnapshot was not
changed. This meant that getCachedSnapshot would always return a promise,
which is truthy, and so hasCachedSnapshot which just checked that the
return value was not null would always return true.

The bug would show up when you tried to navigate back to a page after
a post form submission. The form submission would clear the cache, but the
restoration visit would think it had a cached snapshot and so would not
issue a request. This meant that the page would not be restored and nothing
would happen.

There's a new test in navigation_tests.js that reproduces this bug.

The fix is to make hasCachedSnapshot async and await it in the places
where it is used.

src/core/drive/visit.js
@@ -154,10 +154,10 @@ export class Visit {
     }
   }
 
-  issueRequest() {
+  async issueRequest() {
     if (this.hasPreloadedResponse()) {
       this.simulateRequest()
-    } else if (this.shouldIssueRequest() && !this.request) {
+    } else if (!this.request && await this.shouldIssueRequest()) {
       this.request = new FetchRequest(this, FetchMethod.get, this.location)
       this.request.perform()
     }
@@ -231,14 +231,14 @@ export class Visit {
     }
   }
 
-  hasCachedSnapshot() {
-    return this.getCachedSnapshot() != null
+  async hasCachedSnapshot() {
+    return (await this.getCachedSnapshot()) != null
   }
 
   async loadCachedSnapshot() {
     const snapshot = await this.getCachedSnapshot()
     if (snapshot) {
-      const isPreview = this.shouldIssueRequest()
+      const isPreview = await this.shouldIssueRequest()
       this.render(async () => {
         this.cacheSnapshot()
         if (this.isSamePage) {
@@ -391,11 +391,11 @@ export class Visit {
     return typeof this.response == "object"
   }
 
-  shouldIssueRequest() {
+  async shouldIssueRequest() {
     if (this.isSamePage) {
       return false
-    } else if (this.action == "restore") {
-      return !this.hasCachedSnapshot()
+    } else if (this.action === "restore") {
+      return !(await this.hasCachedSnapshot())
     } else {
       return this.willRender
     }

src/core/native/browser_adapter.js
@@ -22,11 +22,7 @@ export class BrowserAdapter {
 
   visitRequestStarted(visit) {
     this.progressBar.setValue(0)
-    if (visit.hasCachedSnapshot() || visit.action != "restore") {
-      this.showVisitProgressBarAfterDelay()
-    } else {
-      this.showProgressBar()
-    }
+    this.showVisitProgressBarAfterDelay()
   }
 
   visitRequestCompleted(visit) {

src/tests/functional/navigation_tests.js
@@ -195,7 +195,13 @@ test("test following a POST form clears cache", async ({ page }) => {
   await page.click("#form-post-submit")
   await nextBeat() // 301 redirect response
   await nextBeat() // 200 response
+
+  assert.equal(await page.textContent("h1"), "One")
+
   await page.goBack()
+  await nextBeat()
+
+  assert.equal(await page.textContent("h1"), "Navigation")
   assert.notOk(await hasSelector(page, "some-cached-element"))
 })
 