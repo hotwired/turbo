38a5671ab2aa9a9fa92a0e9b38d4ea32d7ae64cf
Alberto Fernández-Capel <afcapel@gmail.com>
2023-11-23 16:12:32 +0000

    Export StreamActions as property of window.Turbo
    This was removed in https://github.com/hotwired/turbo/pull/1049
    but it's still used in the turbo-rails test suite.
    
    https://github.com/hotwired/turbo-rails/blob/c3ca7009c0759563ec65ecf1bc60a9af1ff33728/test/system/broadcasts_test.rb#L137

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import * as Turbo from "./core"
 window.Turbo = Turbo
 Turbo.start()
 
-export { StreamActions } from "./core/streams/stream_actions"
 export * from "./core"
 export * from "./elements"
 export * from "./http"
