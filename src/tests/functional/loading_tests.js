75181bf969ea203c34c5b77eb005638f809bf61c
Paul Shippy <paul.shippy@strongmind.com>
2024-05-29 21:37:21 -0700

    Test for loaded promise on lazy loaded frame

--- src/tests/functional/loading_tests.js
+++ src/tests/functional/loading_tests.js
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test"
+import { test, expect } from "@playwright/test"
 import { assert } from "chai"
 import {
   attributeForSelector,
@@ -15,6 +15,21 @@ import {
 test.beforeEach(async ({ page }) => {
   await page.goto("/src/tests/fixtures/loading.html")
   await readEventLogs(page)
+})
+
+test("when lazy loading, loaded promise does not resolve until it is actually loaded", async ({ page }) => {
+  await nextBeat()
+
+  const frameContents = "#loading-lazy turbo-frame h2"
+  assert.notOk(await hasSelector(page, frameContents))
+  assert.ok(await hasSelector(page, "#loading-lazy turbo-frame:not([complete])"))
+
+  await page.click("#loading-lazy summary")
+  await nextBeat()
+
+  const inputSelector = "#permanent-input-in-frame"
+
+  await expect(page.locator(inputSelector)).toBeFocused()
 })
 
 test("eager loading within a details element", async ({ page }) => {
