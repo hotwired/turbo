a262b720ba082448787047582bce7c585538e6d7
Dom Christie <christiedom@gmail.com>
2024-07-20 21:19:49 +0100

    Clean up unnecessary code now that same-page link clicks are handled outside a Visit

--- src/core/session.js
+++ src/core/session.js
@@ -253,23 +253,13 @@ export class Session {
       this.view.markVisitDirection(visit.direction)
     }
     extendURLWithDeprecatedProperties(visit.location)
-    if (!visit.silent) {
-      this.notifyApplicationAfterVisitingLocation(visit.location, visit.action)
-    }
+    this.notifyApplicationAfterVisitingLocation(visit.location, visit.action)
   }
 
   visitCompleted(visit) {
     this.view.unmarkVisitDirection()
     clearBusyState(document.documentElement)
     this.notifyApplicationAfterPageLoad(visit.getTimingMetrics())
-  }
-
-  locationWithActionIsSamePage(location, action) {
-    return this.navigator.locationWithActionIsSamePage(location, action)
-  }
-
-  visitScrolledToSamePageLocation(oldURL, newURL) {
-    this.notifyApplicationAfterVisitingSamePageLocation(oldURL, newURL)
   }
 
   // Form submit observer delegate
@@ -311,9 +301,7 @@ export class Session {
   // Page view delegate
 
   viewWillCacheSnapshot() {
-    if (!this.navigator.currentVisit?.silent) {
-      this.notifyApplicationBeforeCachingSnapshot()
-    }
+    this.notifyApplicationBeforeCachingSnapshot()
   }
 
   allowsImmediateRender({ element }, options) {
@@ -403,15 +391,6 @@ export class Session {
     return dispatch("turbo:load", {
       detail: { url: this.location.href, timing }
     })
-  }
-
-  notifyApplicationAfterVisitingSamePageLocation(oldURL, newURL) {
-    dispatchEvent(
-      new HashChangeEvent("hashchange", {
-        oldURL: oldURL.toString(),
-        newURL: newURL.toString()
-      })
-    )
   }
 
   notifyApplicationAfterFrameLoad(frame) {
