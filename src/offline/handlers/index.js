9237a8f523b1072286cd45d7fe51e81df57b9490
Rosa Gutierrez <rosa@37signals.com>
2025-08-05 12:42:27 +0200

    Expose different handlers for use with rules

--- src/offline/handlers/index.js
+++ src/offline/handlers/index.js
@@ -1,1 +1,7 @@
-export { CacheFirst } from "./cache_first"
+import { CacheFirst } from "./cache_first"
+import { NetworkFirst } from "./network_first"
+import { StaleWhileRevalidate } from "./stale_while_revalidate"
+
+export const cacheFirst = (config) => new CacheFirst(config)
+export const networkFirst = (config) => new NetworkFirst(config)
+export const staleWhileRevalidate = (config) => new StaleWhileRevalidate(config)
--- src/offline/index.js
+++ src/offline/index.js
@@ -1,7 +1,6 @@
 import { ServiceWorker } from "./service_worker"
 import { config } from "./config"
 
-// Create a serviceWorker instance like how Turbo creates session, navigator, etc.
 const serviceWorker = new ServiceWorker()
 
 export { config, serviceWorker, ServiceWorker }
