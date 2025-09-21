42f49902de92614433cdf0b17cb0ef6a74e01c56
Sean Doyle <sean.p.doyle24@gmail.com>
2025-09-21 19:53:01 -0400

    Consolidate morphing functions into `core/morphing`
    Follow-up to https://github.com/hotwired/turbo/pull/1319
    
    Prior to this commit, the `core/morphing` module defined and exported
    the `morphElements` and `morphChildren`, but did not include the logic
    for `MorphingPageRenderer.render` and `MorphingFrameRenderer.render`.
    
    Instead, those static methods imported and invoked the necessary
    functions.
    
    For the sake of consolidating all morphing logic (`Idiomorph` options,
    event dispatching, etc.), this commit moves the logic to the
    `core/morphing` module, then imports those functions back into the
    `MorphingPageRenderer` class (`morphBodyElements`) and the
    `MorphingFrameRenderer` class (`morphTurboFrameElements`).

--- src/core/frames/morphing_frame_renderer.js
+++ src/core/frames/morphing_frame_renderer.js
@@ -1,32 +1,12 @@
 import { FrameRenderer } from "./frame_renderer"
-import { morphChildren, shouldRefreshFrameWithMorphing, closestFrameReloadableWithMorphing } from "../morphing"
-import { dispatch } from "../../util"
+import { morphTurboFrameElements } from "../morphing"
 
 export class MorphingFrameRenderer extends FrameRenderer {
   static renderElement(currentElement, newElement) {
-    dispatch("turbo:before-frame-morph", {
-      target: currentElement,
-      detail: { currentElement, newElement }
-    })
-
-    morphChildren(currentElement, newElement, {
-      callbacks: {
-        beforeNodeMorphed: (node, newNode) => {
-          if (
-            shouldRefreshFrameWithMorphing(node, newNode) &&
-              closestFrameReloadableWithMorphing(node) === currentElement
-          ) {
-            node.reload()
-            return false
-          }
-          return true
-        }
-      }
-    })
+    morphTurboFrameElements(currentElement, newElement)
   }
 
   async preservingPermanentElements(callback) {
     return await callback()
   }
 }
-
