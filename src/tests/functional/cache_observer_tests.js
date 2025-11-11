8247b71f3a68419c8422c8e36c6dcfeb0d0fa167
Sean Doyle <sean.p.doyle24@gmail.com>
2025-11-10 20:38:26 -0500

    Playwright: replace `assert` with `expect`
    Replace calls to `assert`-prefixed assertions with Playwright's
    `expect`-based assertions in the following files:
    
    * `src/tests/functional/async_script_tests.js`
    * `src/tests/functional/cache_observer_tests.js`
    * `src/tests/functional/drive_disabled_tests.js`
    * `src/tests/functional/drive_tests.js`
    * `src/tests/functional/drive_view_transition_legacy_tests.js`
    * `src/tests/functional/import_tests.js`
    * `src/tests/functional/link_prefetch_observer_tests.js`
    * `src/tests/functional/pausable_rendering_tests.js`
    * `src/tests/functional/preloader_tests.js`
    
    Some `nextBody` calls are made unnecessary by invoking Playwright's
    wait-and-retry expectations, so this commit removes them when possible.

--- src/tests/functional/cache_observer_tests.js
+++ src/tests/functional/cache_observer_tests.js
@@ -1,37 +1,33 @@
-import { test } from "@playwright/test"
-import { assert } from "chai"
-import { hasSelector, nextBody } from "../helpers/page"
+import { expect, test } from "@playwright/test"
+import { nextEventNamed } from "../helpers/page"
 
 test("removes temporary elements", async ({ page }) => {
   await page.goto("/src/tests/fixtures/cache_observer.html")
 
-  assert.equal(await page.textContent("#temporary"), "data-turbo-temporary")
+  await expect(page.locator("#temporary")).toHaveText("data-turbo-temporary")
 
   await page.click("#link")
-  await nextBody(page)
+  await nextEventNamed(page, "turbo:load")
   await page.goBack()
-  await nextBody(page)
 
-  assert.notOk(await hasSelector(page, "#temporary"))
+  await expect(page.locator("#temporary")).not.toBeAttached()
 })
 
 test("removes temporary elements with deprecated turbo-cache=false selector", async ({ page }) => {
   await page.goto("/src/tests/fixtures/cache_observer.html")
 
-  assert.equal(await page.textContent("#temporary-with-deprecated-selector"), "data-turbo-cache=false")
+  await expect(page.locator("#temporary-with-deprecated-selector")).toHaveText("data-turbo-cache=false")
 
   await page.click("#link")
-  await nextBody(page)
+  await nextEventNamed(page, "turbo:load")
   await page.goBack()
-  await nextBody(page)
 
-  assert.notOk(await hasSelector(page, "#temporary-with-deprecated-selector"))
+  await expect(page.locator("#temporary-with-deprecated-selector")).not.toBeAttached()
 })
 
 test("following a redirect renders [data-turbo-temporary] elements before the cache removes", async ({ page }) => {
   await page.goto("/src/tests/fixtures/navigation.html")
   await page.click("#redirect-to-cache-observer")
-  await nextBody(page)
 
-  assert.equal(await page.textContent("#temporary"), "data-turbo-temporary")
+  await expect(page.locator("#temporary")).toHaveText("data-turbo-temporary")
 })
