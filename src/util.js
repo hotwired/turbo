9951bff30f7b9f5c634c78528da83747a8fb808d
Sean Doyle <sean.p.doyle24@gmail.com>
2026-02-02 15:37:25 -0500

    Same-page anchor: Account for `<a>` nested in `<svg>`
    Follow-up to [#1285][]
    
    While an `<a>` element is typically represented by an
    [HTMLAnchorElement][], an `<a>` nested inside an `<svg>` element is
    instead an [SVGAElement][].
    
    Similarly, while [HTMLAnchorElement.href][] returns a `String`, the
    [SVGAElement.href][] returns an instance of [SVGAnimatedString][], which
    cannot be supported by neither calls to [String.startsWith][] nor
    [RegExp.test][].
    
    This commit adds explicit test coverage for a same-page navigation from
    an `<a>` nested within an `<svg>`. To ensure that the value is a
    `String`, replace `.href` with `getAttribute("href")`. This restores the
    implementation to be closer to what was defined in [e591ea9e][].
    
    [#1285]: https://github.com/hotwired/turbo/pull/1285
    [HTMLAnchorElement]: https://developer.mozilla.org/en-US/docs/Web/API/HTMLAnchorElement
    [SVGAElement]: https://developer.mozilla.org/en-US/docs/Web/API/SVGAElement
    [HTMLAnchorElement.href]: https://developer.mozilla.org/en-US/docs/Web/API/HTMLAnchorElement/href
    [SVGAElement.href]: https://developer.mozilla.org/en-US/docs/Web/API/SVGAElement/href
    [SVGAnimatedString]: https://developer.mozilla.org/en-US/docs/Web/API/SVGAnimatedString
    [String.startsWith]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
    [RegExp.test]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
    [e591ea9e]: https://github.com/hotwired/turbo/pull/1285/changes/e591ea9eb087a308f8d6997dc3ffe051058e3ec9#diff-9c0ec1b0a889e599f3ff81590864dd9dc65684a86b41f1da75acc53fafed12e3R251-R256

--- src/util.js
+++ src/util.js
@@ -248,11 +248,13 @@ export function findLinkFromClickTarget(target) {
   const link = findClosestRecursively(target, "a[href], a[xlink\\:href]")
 
   if (!link) return null
-  if (link.href.startsWith("#")) return null
   if (link.hasAttribute("download")) return null
 
   const linkTarget = link.getAttribute("target")
   if (linkTarget && linkTarget !== "_self") return null
+
+  const linkHref = link.getAttribute("href")
+  if (linkHref && linkHref.startsWith("#")) return null
 
   return link
 }
